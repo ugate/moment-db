395a9a26c28921ba60802cd0458f438ea6b8c068
whoover <whoover@nemours.org>
2020-03-04 14:27:45 -0700

    [FEATURE]: Reverse conversions from formatted strings into Dates [FIX]: Various bug fixes

--- README.md
+++ README.md
@@ -33,39 +33,43 @@ Using the forementioned notation, the following database data types can be used 
 | [INTERVAL YEAR TO MONTH](#intervalyeartomonth)    | `[+|-]YEARS-MM`                         | `+130-01`
 | [INTERVAL DAY TO SECONDS](#intervaldaytosecond)   | `[+|-]DAYS HH:MI:SS.FFF`                | `-47482 12:01:20.903`
 
+## Converting ES Dates<sub id="datetostring"></sub>
+
+Each _static_ `MomentDB` function accepts a `Date` that can be formatted into an ANSI compliant date/time format suitible for database consumption.
+
 ### DATE<sub id="date"></sub>
 ```js
-const date = MomentDB.toDate(new Date());
+const date = MomentDB.date(new Date());
 // assumming date, the output would be something like:
-// 2030-01-31 12:01:20.903 -07:00
+// 2030-01-31
 ```
 
 ### TIME (w/o time zone)<sub id="time"></sub>
 ```js
-const time = MomentDB.toTime(new Date(), true);
+const time = MomentDB.time(new Date(), true);
 // assumming date, the output would be something like:
 // 12:01:20.903
 ```
 
 ### TIME (with time zone)<sub id="timezone"></sub>
 ```js
-const time = MomentDB.toTime(new Date());
+const time = MomentDB.time(new Date());
 // assumming date/timezone, the output would be something like:
 // 12:01:20.903 -07:00
 ```
 
 ### TIMESTAMP (w/o time zone)<sub id="timestamp"></sub>
 ```js
-const ts = MomentDB.toTimestamp(new Date(), true);
+const ts = MomentDB.timestamp(new Date(), true);
 // assumming date, the output would be something like:
-// 2030-12-31 12:01:20.903
+// 2030-01-31 12:01:20.903
 ```
 
 ### TIMESTAMP (with time zone)<sub id="timestampzone"></sub>
 ```js
-const ts = MomentDB.toTimestamp(new Date());
+const ts = MomentDB.timestamp(new Date());
 // assumming date/timezone, the output would be something like:
-// 2030-12-31 12:01:20.903 -07:00
+// 2030-01-31 12:01:20.903 -07:00
 ```
 
 ### INTERVAL (year to month)<sub id="intervalyeartomonth"></sub>
@@ -73,7 +77,7 @@ const ts = MomentDB.toTimestamp(new Date());
 const date1 = new Date(Date.UTC(1900, 00, 01)), date2 = new Date();
 const monthDate = new Date();
 monthDate.setMonth(0);
-const interval = MomentDB.toIntervalYearToMonth(date1, date2, monthDate);
+const interval = MomentDB.intervalYearToMonth(date1, date2, monthDate);
 // output (assuming we are in year 2030):
 // +130-01
 ```
@@ -82,7 +86,96 @@ const interval = MomentDB.toIntervalYearToMonth(date1, date2, monthDate);
 ```js
 const date1 = new Date(), date2 = new Date(Date.UTC(1900, 00, 01));
 const timeDate = new Date();
-const interval = MomentDB.toIntervalDayToSecond(date1, date2, timeDate);
+const interval = MomentDB.intervalDayToSecond(date1, date2, timeDate);
 // output (assuming we are in year 2030 and the time is the as indicated in timeDate):
 // -47482 12:01:20.903
+```
+
+## Converting formatted dates/times into ES Dates<sub id="stringtodate"></sub>
+
+Each _static_ `MomentDB` function accepts a string returned from prior call to one of the [string conversion functions](#datetostring) that will be converted back into a `Date`.
+
+### DATE<sub id="date2"></sub>
+```js
+const date = MomentDB.date('2030-01-31');
+const iso = date.toISOString();
+// outputs a Date with the date set as UTC
+// 2030-01-31T00:00:00.000Z
+```
+
+### TIME (w/o time zone)<sub id="time2"></sub>
+```js
+const time = MomentDB.time('12:01:20.903');
+const iso = time.toISOString();
+// outputs a Date with the time set as UTC
+// iso output (date defaults to 0000-01-01):
+// 0000-01-01T12:01:20.903Z
+
+// can also remove the timezone, if needed:
+const time2 = MomentDB.time('12:01:20.903 -07:00', true);
+const iso2 = time2.toISOString();
+// outputs a Date with the time set as UTC (no timezone)
+// iso2 output (date defaults to 0000-01-01):
+// 0000-01-01T12:01:20.903Z
+```
+
+### TIME (with time zone)<sub id="timezone2"></sub>
+```js
+const time = MomentDB.time('12:01:20.903 -07:00');
+const iso = time.toISOString();
+// outputs a Date with the time set with the indicated zone
+// iso output (date defaults to 0000-01-01):
+// 0000-01-01T19:29:38.903Z
+```
+
+### TIMESTAMP (w/o time zone)<sub id="timestamp2"></sub>
+```js
+const ts = MomentDB.timestamp('2030-01-31 12:01:20.903');
+const iso = ts.toISOString();
+// outputs a Date with the date/time set as UTC
+// iso output:
+// 2030-01-31T12:01:20.903Z
+
+// can also remove the timezone, if needed:
+const ts2 = MomentDB.timestamp('2030-01-31 12:01:20.903 -07:00', true);
+const iso2 = ts2.toISOString();
+// outputs a Date with the date/time set as UTC (no timezone)
+// iso2 output:
+// 2030-01-31T12:01:20.903Z
+```
+
+### TIMESTAMP (with time zone)<sub id="timestampzone2"></sub>
+```js
+const ts = MomentDB.timestamp('2030-01-31 12:01:20.903 -07:00');
+const iso = ts.toISOString();
+// iso output:
+// 2030-01-31T19:01:20.903Z
+```
+
+### INTERVAL (year to month)<sub id="intervalyeartomonth2"></sub>
+```js
+const date = MomentDB.intervalYearToMonth('+130-02');
+const iso = date.toISOString();
+// iso output (assuming the current year is 2030):
+// 2160-02-01T00:00:00.000Z
+
+// can also pass a reference date for the year instead of the default current date
+const date2 = MomentDB.intervalYearToMonth('+130-02', new Date(Date.UTC(2030)));
+const iso = date.toISOString();
+// iso output (assuming today is 2030-02-03 and time in UTC is as indicated):
+// 2160-02-01T00:00:00.000Z
+```
+
+### INTERVAL (day to second)<sub id="intervaldaytosecond2"></sub>
+```js
+const date = MomentDB.intervalDayToSecond('-47482 12:01:20.903');
+const iso = date.toISOString();
+// iso output (assuming today is 2030-02-03 and time in UTC is as indicated):
+// 1900-02-03T12:01:20.903Z
+
+// can also pass a reference date to use instead of the default current date
+const date2 = MomentDB.intervalDayToSecond('-47482 12:01:20.903', new Date(Date.UTC(2030, 1, 3)));
+const iso = date.toISOString();
+// iso output (assuming today is 2030-02-03 and time in UTC is as indicated):
+// 1900-02-03T12:01:20.903Z
 ```
--- index.js
+++ index.js
@@ -5,11 +5,11 @@ const TIME = 'HH:MI:SS.FFF';
 const ZONE = '[+|-]TH:TM';
 const YEAR_TO_MONTH = '[+|-]YEARS-MM';
 const DAY_TO_SEC = '[+|-]DAYS HH:MI:SS.FFF';
-const DATE_RX = /(\d{4})-(\d{2})-(\d{2})/;
+const DATE_RX = /(\d{4})-(\d{1,2})-(\d{1,2})/;
 const TIME_RX = /(\d{2}):(\d{2}):(\d{2})\.(\d{3})/;
-const ZONE_RX = /([+-])(\d{2}):(\d{2})/;
-const YEAR_TO_MONTH_RX = /([+-])(\d{4}):(\d{2})/;
-const DAY_TO_SEC_RX = /([+-])(\d{7})/;
+const ZONE_RX = /([+-])(\d{1,2}):(\d{1,2})/;
+const YEAR_TO_MONTH_RX = /([+-]\d{1,4})-(\d{1,2})/;
+const DAY_TO_SEC_RX = /([+-]\d{1,7})/;
 
 /**
  * Moment implementation for formatting Dates into ANSI compatible strings suitable for database consumption.
@@ -32,60 +32,89 @@ export default class MomentDB {
 
   /**
    * Formats the specified date into an ANSI compatible __date__ that is suitable for database consumption
-   * (i.e. `YYYY-MM-DD`; see {@link MomentDB} for notation details).
-   * @param {Date} date The date to extract the __date__ from
-   * @returns {String} The formatted output
+   * (i.e. `YYYY-MM-DD`; see {@link MomentDB} for notation details) or when a formatted Date string is provided, a Date is returned.
+   * @param {(Date | String)} date The date to extract the __date__ from or a formatted date string to convert _back_ into a Date
+   * @returns {(String | Date)} The formatted output when a Date was passed or a Date when a formatted date string was passed
    */
   static date(date) {
-    return format(DATE, date);
+    if (date instanceof Date) {
+      return format(DATE, date);
+    }
+    return unformat([DATE_RX], date);
   }
 
   /**
    * Formats the specified date into an ANSI compatible __time__ (with or w/o a timezone) that is suitable for database consumption
-   * (i.e. `HH:MI:SS.FFF [+|-]TH:TM` or `HH:MI:SS.FFF` w/o a timezone; see {@link MomentDB} for notation details).
-   * @param {Date} date The date to extract the __time__ from
+   * (i.e. `HH:MI:SS.FFF [+|-]TH:TM` or `HH:MI:SS.FFF` w/o a timezone; see {@link MomentDB} for notation details) or when a
+   * formatted date string is provided, a Date is returned.
+   * @param {(Date | String)} date The date to extract the __time__ from or a formatted __time__ string to convert _back_ into a Date
    * @param {Boolean} [excludeTimezone] Truthy to exclude the timezone in the output
-   * @returns {String} The formatted output
+   * @returns {(String | Date)} The formatted output when a Date was passed or a Date when a formatted date string was passed
    */
   static time(date, excludeTimezone) {
-    return format(excludeTimezone ? TIME : `${TIME} ${ZONE}`, date);
+    if (date instanceof Date) {
+      return format(excludeTimezone ? TIME : `${TIME} ${ZONE}`, date);
+    }
+    return unformat(excludeTimezone ? [TIME_RX] : [TIME_RX, ZONE_RX], date);
   }
 
   /**
    * Formats the specified date into an ANSI compatible __timestamp__ (with or w/o a timezone) that is suitable for database consumption
    * (i.e. `YYYY-MM-DD HH:MI:SS.FFF [+|-]TH:TM` or `YYYY-MM-DD HH:MI:SS.FFF` w/o a timezone; see {@link MomentDB} for notation details).
-   * @param {Date} date The date to extract the __timestamp__ from
+   * @param {(Date | String)} date The date to extract the __timestamp__ from or a formatted __timestamp__ string to convert _back_ into
+   * a Date
    * @param {Boolean} [excludeTimezone] Truthy to exclude the timezone in the output
-   * @returns {String} The formatted output
+   * @returns {(String | Date)} The formatted output when a Date was passed or a Date when a formatted date string was passed
    */
   static timestamp(date, excludeTimezone) {
-    return format(`${DATE} ${TIME}${excludeTimezone ? '' : ` ${ZONE}`}`, date);
+    if (date instanceof Date) {
+      return format(`${DATE} ${TIME}${excludeTimezone ? '' : ` ${ZONE}`}`, date);
+    }
+    return unformat(excludeTimezone ? [DATE_RX, TIME_RX] : [DATE_RX, TIME_RX, ZONE_RX], date);
   }
 
   /**
    * Calulates the number of __years__ from the specified `startDate` and `endDate` along with the extracted/formatted __month__
    * from the `monthDate`. The output is rendered in an ANSI compatible format that is suitable for database consumption
-   * (i.e. `[+|-]YEARS-MM`; see {@link MomentDB} for notation details).
-   * @param {Date} startDate The starting date (can be _after_ `endDate` for negative formatting)
-   * @param {Date} endDate The ending date (can be _before_ `startDate` for negative formatting)
-   * @param {Date} monthDate The date to extract the __month__ from
-   * @returns {String} The formatted output
+   * (i.e. `[+|-]YEARS-MM`; see {@link MomentDB} for notation details). When an interval-year-to-month formatted string is passed
+   * as the `startDate`, a Date will be created based upon the number of years and the UTC month within the formatted string.
+   * @param {(Date | String]} startDate The starting date (can be _after_ `endDate` for negative formatting) when generating a 
+   * formatted string _OR_ an interval-year-to-month formatted string to generate a Date from.
+   * @param {Date} [endDate] The ending date (can be _before_ `startDate` for negative formatting). __Required when `startDate`
+   * is an actual Date__. Optional reference Date when converting an interval-year-to-month formatted string to a Date (default
+   * `new Date()` in this case).
+   * @param {Date} [monthDate] The date to extract the __month__ from. __Required when `startDate` is an actual Date (ignored
+   * otherwise)__.
+   * @returns {(String | Date)} The interval-day-to-second formatted string when a Date was passed as `startDate` _OR_ a Date when an
+   * interval-year-to-month formatted string was passed as the `startDate`.
    */
   static intervalYearToMonth(startDate, endDate, monthDate) {
-    return format(YEAR_TO_MONTH, startDate, endDate, monthDate);
+    if (startDate instanceof Date && endDate instanceof Date && monthDate instanceof Date) {
+      return format(YEAR_TO_MONTH, startDate, endDate, monthDate);
+    }
+    return unformat([YEAR_TO_MONTH_RX], startDate, endDate);
   }
 
   /**
    * Calulates the number of __days__ from the specified `startDate` and `endDate` along with the extracted/formatted __time__
    * from the `timestamp`. The output is rendered in an ANSI compatible format that is suitable for database consumption
-   * (i.e. `[+|-]DAYS HH:MI:SS.FFF`; see {@link MomentDB} for notation details).
-   * @param {Date} startDate The starting date (can be _after_ `endDate` for negative formatting)
-   * @param {Date} endDate The ending date (can be _before_ `startDate` for negative formatting)
-   * @param {Date} timestamp The date to extract the __time__ from
-   * @returns {String} The formatted output
+   * (i.e. `[+|-]DAYS HH:MI:SS.FFF`; see {@link MomentDB} for notation details). When an interval-day-to-second formatted string is
+   * passed as the `startDate`, a Date will be created based upon the number of days and the UTC time within the formatted string.
+   * @param {(Date | String)} startDate he starting date (can be _after_ `endDate` for negative formatting) when generating a 
+   * formatted string _OR_ an interval-day-to-second formatted string to generate a Date from.
+   * @param {Date} [endDate] The ending date (can be _before_ `startDate` for negative formatting). __Required when `startDate`
+   * is an actual Date__. Optional reference Date when converting an interval-day-to-second formatted string to a Date (default
+   * `new Date()` in this case).
+   * @param {Date} [timestamp] The date to extract the __time__ from. __Required when `startDate` is an actual Date (ignored
+   * otherwise)__.
+   * @returns {(String | Date)} The interval-day-to-second formatted string when a Date was passed as `startDate` _OR_ a Date when an
+   * interval-day-to-second formatted string was passed as the `startDate`.
    */
   static intervalDayToSecond(startDate, endDate, timestamp) {
-    return format(DAY_TO_SEC, startDate, endDate, timestamp);
+    if (startDate instanceof Date && endDate instanceof Date && timestamp instanceof Date) {
+      return format(DAY_TO_SEC, startDate, endDate, timestamp);
+    }
+    return unformat([DAY_TO_SEC_RX, TIME_RX], startDate, endDate);
   }
 }
 
@@ -122,87 +151,73 @@ function format(format, ...dates) {
     (date || time || zone) && intvl ? ' ' : ''}${intvl}`;
 }
 
-function unformat(formatted) {
-  /*const DATE = 'YYYY-MM-DD';
-  const TIME = 'HH:MI:SS.FFF';
-  const ZONE = '[+|-]TH:TM';
-  const YEAR_TO_MONTH = '[+|-]YEARS-MM';
-  const DAY_TO_SEC = '[+|-]DAYS HH:MI:SS.FFF';*/
-  const DATE_RX = /(\d{4})-(\d{1,2})-(\d{1,2})/;
-  const TIME_RX = /(\d{2}):(\d{2}):(\d{2})\.(\d{3})/;
-  const ZONE_RX = /([+-])(\d{1,2}):(\d{1,2})/;
-  const YEAR_TO_MONTH_RX = /([+-]\d{1,4})-(\d{1,2})/;
-  const DAY_TO_SEC_RX = /([+-]\d{1,7})/;
+/**
+ * Converts a previously formatted date/time from {@link format} into a Date.
+ * @param {RegExp[]} rxs The regular expressions that will indicate how the Date will be set (should match one or more of the
+ * global regular expressions)
+ * @param {String} formatted The formatted date, time, timestamp, etc.
+ * @param {Date} [refDate] A reference date to use when calculating __interval-year-to-month__ or __interval-day-to-second__
+ * (otherwise, ignored)
+ * @returns {Date} A Date set to the proper date/time/zone indicated by the specified regular expressions
+ */
+function unformat(rxs, formatted, refDate) {
+  const dte = rxs.includes(DATE_RX) && formatted.match(DATE_RX);
+  const tms = rxs.includes(TIME_RX) && formatted.match(TIME_RX);
+  const zns = rxs.includes(ZONE_RX) && formatted.match(ZONE_RX);
+  const ytm = rxs.includes(YEAR_TO_MONTH_RX) && formatted.match(YEAR_TO_MONTH_RX);
+  const dts = rxs.includes(DAY_TO_SEC_RX) && formatted.match(DAY_TO_SEC_RX);
 
-  const dte = formatted.match(DATE_RX);
-  const tms = formatted.match(TIME_RX);
-  const zns = dte || tms ? formatted.match(ZONE_RX) : null;
-  const ytm = !tms && !zns ? formatted.match(YEAR_TO_MONTH_RX) : null;
-  const dts = tms && !zns && !ytm ? formatted.match(DAY_TO_SEC_RX) : null;
-
-  let date, yyyy, mm, dd, hh, mi, ss, fff;
+  let date, time, yyyy, mm, dd, hh, mi, ss, fff;
   if (ytm) {
     const years = parseInt(ytm[1]);
-    date = new Date(new Date().getTime() + years * 3.1556952e+10);
+    date = new Date((refDate instanceof Date ? refDate : new Date()).getTime() + years * 3.1556952e+10);
     mm = parseInt(ytm[2]) - 1;
-  } else if (dts || tms) {
+    dd = 1;
+    hh = mi = ss = fff = 0;
+  }
+  if (dts) {
+    const days = parseInt(dts[1]);
+    time = (refDate instanceof Date ? refDate : new Date()).getTime() + days * 8.64e+7;
+    date = new Date(time);
+  }
+  if (dte) {
+    yyyy = parseInt(dte[1]);
+    mm = parseInt(dte[2]) - 1;
+    dd = parseInt(dte[3]);
+    hh = mi = ss = fff = 0;
+  } else if (tms && !date) {
+    yyyy = 0;
+    mm = 0;
+    dd = 1;
+  }
+  if (tms) {
     hh = parseInt(tms[1]);
     mi = parseInt(tms[2]);
     ss = parseInt(tms[3]);
     fff = parseInt(tms[4]);
-    if (dts) {
-      const days = parseInt(dts[1]);
-      date = new Date(new Date().getTime() + days * 8.64e+7);
-    } else {
-      date = new Date();
-    }
   }
-  if (date) {
-    if (yyyy !== undefined) date.setUTCFullYear(yyyy);
-    if (mm !== undefined) date.setUTCMonth(mm);
-    if (dd !== undefined) date.setUTCDate(dd);
-    if (hh !== undefined) date.setUTCHours(hh);
-    if (mi !== undefined) date.setUTCMinutes(mi);
-    if (ss !== undefined) date.setUTCSeconds(ss);
-    if (fff !== undefined) date.setUTCMilliseconds(fff);
-  } else if (dte) {
-    yyyy = parseInt(dte[1]);
-    mm = parseInt(dte[2]);
-    dd = parseInt(dte[3]);
-    date = new Date(`${yyyy}-${mm}-${dd}${tms ? `T${hh}:${mi}:${ss}.${fff}${zns ? `${zns[1]}${zns[2]}:${zns[3]}` : ''}` : ''}`);
-  }
+  const type = zns ? '' : 'UTC';
+  if (zns) {
+    if (!time) time = 0;
+    if (hh !== undefined) time += hh * 3.6e+6;
+    if (mi !== undefined) time += mi * 60000;
+    if (ss !== undefined) time += ss * 1000;
+    if (fff !== undefined) time += fff;
+    const offset = (zns[1] === '-' ? -1 : 1) * (parseInt(zns[2]) * 3.6e+6 + parseInt(zns[3]) * 60000);
+    if (time) time -= offset;
+    else time = offset;
+    if (date) date.setTime(time);
+    else date = new Date(time);
+    hh = mi = ss = fff = undefined;
+  } else if (!date) date = new Date();
+  if (yyyy !== undefined) date[`set${type}FullYear`](yyyy);
+  if (mm !== undefined) date[`set${type}Month`](mm);
+  if (dd !== undefined) date[`set${type}Date`](dd);
+  if (hh !== undefined) date[`set${type}Hours`](hh);
+  if (mi !== undefined) date[`set${type}Minutes`](mi);
+  if (ss !== undefined) date[`set${type}Seconds`](ss);
+  if (fff !== undefined) date[`set${type}Milliseconds`](fff);
   return date;
-
-  const offset = zns ? parseInt(zns[1]) * 3.6e+6 + parseInt(zns[2]) * 60000 : null;
-}
-
-function humanise(dayCount) {
-  const now = new Date(), then = new Date(now.getTime() + dayCount * 8.64e+7);
-  const years = then.getUTCFullYear() - now.getUTCFullYear();
-  const months = then.getUTCMonth() - now.getUTCMonth();
-  const days = then.getUTCDate() - now.getUTCDate();
-  return `${years}-${months}-${days}`;
-
-  const cal = [31, (then.getUTCFullYear()%4?29:28), 31, 30, 31, 30, 31, 31, 30, 31, 30, 31];
-    var date_string = "";
-    while(true)
-    {
-        date_string = "";
-        date_string += (years>0?years + "Y":"");
-
-        if(months<0){
-          years -= 1; months += 12; continue;
-        }
-        date_string += (months>0?months + "M":"");
-
-        if(days<0){
-          months -= 1; days += cal[((11+then.getUTCMonth())%12)]; continue;
-        }
-        date_string += (days>0?days + "D":"");
-        break;
-    }
-    console.log(date_string);
-    return date_string;
 }
 
 /**
